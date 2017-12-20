172076449c013f76119205845c61714bf8a632b2
Roger Berlind <roger@hashicorp.com>
2017-12-19 23:12:15 -0500

    added main.tf

---
+++ main.tf
@@ -0,0 +1,36 @@
+variable "credentials" {
+  description = "GCP credentials"
+}
+
+variable "gcp_project" {
+  description = "GCP project name"
+}
+
+variable "gcp_region" {
+  description = "GCP region, e.g. us-east1"
+  default = "us-east1"
+}
+
+
+provider "google" {
+  credentials = "${var.credentials}"
+  project     = "${var.gcp_project}"
+  region      = "${var.gcp_region}"
+}
+
+resource "google_compute_network" "default" {
+  name                    = "roger-berlind-network"
+  auto_create_subnetworks = "true"
+}
+
+resource "google_compute_firewall" "default" {
+  name    = "roger-berlind-firewall"
+  network = "${google_compute_network.default.name}"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_ranges = "["0.0.0.0/0"]"
+}
